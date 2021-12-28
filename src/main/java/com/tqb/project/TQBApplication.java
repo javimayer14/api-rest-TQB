bb5d5dffc9a7875e8c8293985c5f0d2ba2c580f2
Mayer <mayer@localhost.localdomain>
2021-12-28 11:25:42 -0300

    agregando configuracion de auditoria

--- src/main/java/com/tqb/project/TQBApplication.java
+++ src/main/java/com/tqb/project/TQBApplication.java
@@ -3,10 +3,12 @@ package com.tqb.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class TQBApplication {
 
